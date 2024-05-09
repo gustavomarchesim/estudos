c4b6715160d4c5be34822523a8f0e9282383b071
gustavomarchesim <gustavomarchesim@outlook.com>
2024-05-09 17:03:18 -0300

    Default methods with interfaces

--- naj235/src/application/Program.java
+++ naj235/src/application/Program.java
@@ -3,7 +3,8 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import services.BrazilInterestService;
+import services.InterestService;
+import services.UsaInterestService;
 
 public class Program {
     public static void main(String[] args) {
@@ -18,7 +19,7 @@ public class Program {
         System.out.print("Months: ");
         months = sc.nextInt();
 
-        BrazilInterestService is = new BrazilInterestService(2.0);
+        InterestService is = new UsaInterestService(1.0);
         payment = is.payment(amount, months);
 
         System.out.println("Payment after " + months + " months:");
