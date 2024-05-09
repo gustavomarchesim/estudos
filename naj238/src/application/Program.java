318ad7c0f09b390aed0b8583252a0b553212b19c
gustavomarchesim <gustavomarchesim@outlook.com>
2024-05-09 18:09:54 -0300

    Generics with type safety

--- naj238/src/application/Program.java
+++ naj238/src/application/Program.java
@@ -8,12 +8,10 @@ public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        PrintService ps = new PrintService();
+        PrintService<Integer> ps = new PrintService<>();
 
         System.out.print("How many values? ");
         int n = sc.nextInt();
-
-        ps.addValue("Maria");
 
         for (int i = 0; i < n; i++) {
             int value = sc.nextInt();
@@ -21,8 +19,7 @@ public class Program {
         }
 
         ps.print();
-        Integer x = (Integer) ps.first();
-        System.out.println("First: " + x);
+        System.out.println("First: " + ps.first());
 
         sc.close();
     }
