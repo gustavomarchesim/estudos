16f32980e6a6ef273cf270939c98cca57436d624
gustavomarchesim <gustavomarchesim@outlook.com>
2024-05-09 21:29:27 -0300

    Tipo coringa

---
+++ naj240/src/Program.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+
+        List<?> myObjs = new ArrayList<Object>();
+        List<Integer> myNumbers = new ArrayList<Integer>();
+
+        Object obj;
+        Integer x = 10;
+        obj = x;
+    }
+
+}
