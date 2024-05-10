5030e49c3a82bddf6a8d72fcc05e59ea325a3e34
gustavomarchesim <gustavomarchesim@outlook.com>
2024-05-10 15:40:09 -0300

    Exemplo de erro de compilação

--- naj240/src/Program.java
+++ naj240/src/Program.java
@@ -11,6 +11,8 @@ public class Program {
     }
 
     public static void printList(List<?> list) {
+        list.add(3); // Não podemos adicionar elementos para tipos coringa, pois o compilador não
+                     // sabe quem é quem
         for (Object obj : list) {
             System.out.println(obj);
         }
