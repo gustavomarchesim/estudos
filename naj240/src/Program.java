7f8f37f53b01c0603381217c8f8691ecfe14f3f9
gustavomarchesim <gustavomarchesim@outlook.com>
2024-05-10 15:38:04 -0300

    Exemplo de uso Tipo coringa

--- naj240/src/Program.java
+++ naj240/src/Program.java
@@ -1,15 +1,19 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
+        List<Integer> myInts = Arrays.asList(5, 2, 10);
+        printList(myInts);
 
-        List<?> myObjs = new ArrayList<Object>();
-        List<Integer> myNumbers = new ArrayList<Integer>();
+        List<String> myStrs = Arrays.asList("Maria, Alex, Bob");
+        printList(myStrs);
+    }
 
-        Object obj;
-        Integer x = 10;
-        obj = x;
+    public static void printList(List<?> list) {
+        for (Object obj : list) {
+            System.out.println(obj);
+        }
     }
 
 }
