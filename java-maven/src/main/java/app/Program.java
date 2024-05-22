ef05fb89f394206a9a77646deaa1a4e48dc5a98e
Gustavo Marchesim <gustavomarchesim@outlook.com>
2024-05-22 17:12:09 -0300

    Exemplo de busca usando find na base de dados

--- java-maven/src/main/java/app/Program.java
+++ java-maven/src/main/java/app/Program.java
@@ -9,20 +9,13 @@ import dominio.Pessoa;
 public class Program {
     public static void main(String[] args) {
 
-        Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-        Pessoa p2 = new Pessoa(null, "Mariano da Silva", "mariano@gmail.com");
-        Pessoa p3 = new Pessoa(null, "Tainara da Silva", "tainara@gmail.com");
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
 
-        em.getTransaction().begin();
+        Pessoa p = em.find(Pessoa.class, 3);
 
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
+        System.out.println(p);
 
-        em.getTransaction().commit();
         em.close();
         emf.close();
 
